00f484a24e60995364e20bd6ac1a984575df2c06
petak5 <peterurgos12345@gmail.com>
2020-04-03 21:21:18 +0200

    Added Flask Marshmallow to the project, implemented get_all_users endpoint.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,7 +3,9 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_marshmallow import Marshmallow
 app = Flask(__name__)
+ma = Marshmallow(app)
 
 from api.models import db
 import api.models
