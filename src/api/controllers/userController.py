45c0794ffd0277dee128c4d48251cd4cdbfb07ed
petak5 <peterurgos12345@gmail.com>
2020-04-05 13:27:04 +0200

    Fixed User's get and post routes and added a delete route.

--- src/api/controllers/userController.py
+++ src/api/controllers/userController.py
@@ -7,12 +7,21 @@ class UserController:
         self.session.add(user)
         self.session.commit()
 
+        return user
+
     def get_user(self, **kwargs):
         user = User.query.filter_by(**kwargs).first()
+
         return user
-    
+
     def get_all_users(self, **kwargs):
         all_users = User.query.all()
+
         return all_users
+
+    def delete_user(self, id):
+        result = User.query.filter_by(id=id).delete()
+        db.session.commit()
+        return result
 
 userController = UserController()
