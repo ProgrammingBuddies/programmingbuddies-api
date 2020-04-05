273587604cc48cefa47b3ca8824d6157b4265dde
petak5 <peterurgos12345@gmail.com>
2020-04-05 13:27:04 +0200

    Fixed User's get and post routes and added a delete route.

--- src/api/views/userView.py
+++ src/api/views/userView.py
@@ -6,18 +6,32 @@ from api.models.userModel import UserSchema
 userSchema = UserSchema()
 usersSchema = UserSchema(many=True)
 
-@app.route("/user/push", methods=['POST'])
-def push_user():
-    user = userController.create_user(**request.form)
-    return "200OK"
+@app.route("/users", methods=['POST'])
+def post_user():
+    user = userController.create_user(**request.get_json())
 
-@app.route("/user/get/<name>", methods=['GET'])
-def get_user(name):
-    user = userController.get_user(name=name)
-    return user.name
+    return jsonify(userSchema.dump(user)), 201
+
+@app.route("/users/<id>", methods=['GET'])
+def get_user(id):
+    user = userController.get_user(id=id)
+
+    if user:
+        return jsonify(userSchema.dump(user)), 200
+    else:
+        return "", 404
 
 @app.route("/users", methods=['GET'])
 def get_all_users():
     all_users = userController.get_all_users()
 
-    return jsonify(usersSchema.dump(all_users))
+    return jsonify(usersSchema.dump(all_users)), 200
+
+@app.route("/users/<id>", methods=['DELETE'])
+def delete_user(id):
+    result = userController.delete_user(id)
+
+    if result:
+        return "", 202
+    else:
+        return "", 404
