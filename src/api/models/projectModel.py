501f220a2a00138094f95a4a68b851009d51cb54
petak5 <peterurgos12345@gmail.com>
2020-04-11 20:37:14 +0200

    Removed Flask Marshmallow and replaced it with manual iterating over model properties. Fixed users delete route.

--- src/api/models/projectModel.py
+++ src/api/models/projectModel.py
@@ -11,6 +11,19 @@ class Project(db.Model):
     users = db.relationship('UserHasProject', back_populates='project')
     links = db.relationship('ProjectLink', backref='project', lazy=True)
 
+    def as_dict(self):
+        obj_d = {
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'languages': self.languages,
+            'development_status': self.development_status,
+            'creation_date': self.creation_date,
+            'release_date': self.release_date,
+            'links': [ link.as_dict() for link in self.links ]
+        }
+        return obj_d
+
     def __repr__(self):
         return '<Project %r>' % self.name
 
@@ -19,6 +32,16 @@ class ProjectLink(db.Model):
     name = db.Column(db.String(80), nullable=False)
     url = db.Column(db.Text, nullable=False)
     project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+
+    # todo: decide if this should contain project_id (it is already present in the model)
+    def as_dict(self):
+        obj_d = {
+            'id': self.id,
+            'name': self.name,
+            'url': self.url,
+            'project_id': self.project_id
+        }
+        return obj_d
 
     def __repr__(self):
         return '<ProjectLink %r>' % self.name
--- src/api/views/userView.py
+++ src/api/views/userView.py
@@ -1,23 +1,19 @@
 from flask import request, jsonify
 from api import app
 from api.controllers import userController
-from api.models.userModel import UserSchema
-
-userSchema = UserSchema()
-usersSchema = UserSchema(many=True)
 
 @app.route("/users", methods=['POST'])
 def post_user():
     user = userController.create_user(**request.get_json())
 
-    return jsonify(userSchema.dump(user)), 201
+    return jsonify(user.as_dict()), 201
 
 @app.route("/users/<id>", methods=['GET'])
 def get_user(id):
     user = userController.get_user(id=id)
 
     if user:
-        return jsonify(userSchema.dump(user)), 200
+        return jsonify(user.as_dict()), 200
     else:
         return "", 404
 
@@ -25,13 +21,15 @@ def get_user(id):
 def get_all_users():
     all_users = userController.get_all_users()
 
-    return jsonify(usersSchema.dump(all_users)), 200
+    users = [ user.as_dict() for user in all_users ]
+
+    return jsonify(users), 200
 
 @app.route("/users/<id>", methods=['DELETE'])
 def delete_user(id):
-    result = userController.delete_user(id)
+    user = userController.delete_user(id)
 
-    if result:
+    if user:
         return "", 202
     else:
         return "", 404
