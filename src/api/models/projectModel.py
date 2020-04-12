05f21b5847d2f7905ea3d7202db9dfe34a5b4cbf
petak5 <peterurgos12345@gmail.com>
2020-04-12 09:52:54 +0200

    Implemented Project routes.

--- src/api/models/projectModel.py
+++ src/api/models/projectModel.py
@@ -1,12 +1,13 @@
 from api.models import db
+from datetime import datetime
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     description = db.Column(db.Text, nullable=False)
     languages = db.Column(db.Text)
-    development_status = db.Column(db.Integer, nullable=False)
-    creation_date = db.Column(db.DateTime, nullable=False)
+    development_status = db.Column(db.Integer, nullable=False, default=0)
+    creation_date = db.Column(db.DateTime, nullable=False, default=datetime.now())
     release_date = db.Column(db.DateTime)
     users = db.relationship('UserHasProject', back_populates='project')
     links = db.relationship('ProjectLink', backref='project', lazy=True)
@@ -20,6 +21,7 @@ class Project(db.Model):
             'development_status': self.development_status,
             'creation_date': self.creation_date,
             'release_date': self.release_date,
+            'users': [ user.user_as_dict() for user in self.users ],
             'links': [ link.as_dict() for link in self.links ]
         }
         return obj_d
--- src/api/views/__init__.py
+++ src/api/views/__init__.py
@@ -1,4 +1,4 @@
 """
 The views package
 """
-from api.views import userView, oauthView
+from api.views import userView, projectView, oauthView
