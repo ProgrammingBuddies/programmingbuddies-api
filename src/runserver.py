a15ed7fd4ad08b9523f6b199aaa4b44cb5176239
qaz wsx <cgeocodgod@gmail.com>
2020-04-16 15:01:08 +0300

    ~ Server now generates a random key on startup

--- src/runserver.py
+++ src/runserver.py
@@ -4,6 +4,7 @@ This script runs the api application using a development server.
 
 from os import environ
 from api import app
+from secrets import token_urlsafe
 
 if __name__ == '__main__':
     HOST = environ.get('SERVER_HOST', 'localhost')
@@ -11,5 +12,5 @@ if __name__ == '__main__':
         PORT = int(environ.get('SERVER_PORT', '5001'))
     except ValueError:
         PORT = 5001
-    app.config["SECRET_KEY"] = "OCML3BRawWEUeaxcuKHLpw"
+    app.config["SECRET_KEY"] = token_urlsafe(16)
     app.run(HOST, PORT)
