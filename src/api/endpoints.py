e0e403d53406d9f68cd642c408d83fce73e8dcb4
petak5 <peterurgos12345@gmail.com>
2020-05-23 08:39:58 +0200

    Add missing dependencies to  route.

--- src/api/endpoints.py
+++ src/api/endpoints.py
@@ -27,20 +27,38 @@ def version():
             description: Everything went alright
     """
     return jsonify({
+        "python_version": "{}.{}".format(version_info.major, version_info.minor),
         "pip_version": "{}".format(pkg_resources.get_distribution('pip').version),
+        "flask-dance_version": "{}".format(pkg_resources.get_distribution('flask-dance').version),
+        "oauthlib_version": "{}".format(pkg_resources.get_distribution('oauthlib').version),
+        "requests_version": "{}".format(pkg_resources.get_distribution('requests').version),
+        "certifi_version": "{}".format(pkg_resources.get_distribution('certifi').version),
+        "cherdet_version": "{}".format(pkg_resources.get_distribution('chardet').version),
+        "idna_version": "{}".format(pkg_resources.get_distribution('idna').version),
+        "urllib3_version": "{}".format(pkg_resources.get_distribution('urllib3').version),
+        "urlobject_version": "{}".format(pkg_resources.get_distribution('urlobject').version),
         "pyopenssl_version": "{}".format(pkg_resources.get_distribution('pyopenssl').version),
+        "flask-sqlalchemy_version": "{}".format(pkg_resources.get_distribution('flask-sqlalchemy').version),
+        "sqlalchemy_version": "{}".format(pkg_resources.get_distribution('sqlalchemy').version),
+        "sqlalchemy-utils_version": "{}".format(pkg_resources.get_distribution('sqlalchemy-utils').version),
+        "flask-swagger_version": "{}".format(pkg_resources.get_distribution('flask-swagger').version),
+        "pyyaml_version": "{}".format(pkg_resources.get_distribution('pyyaml').version),
+        "flask-swagger-ui_version": "{}".format(pkg_resources.get_distribution('flask-swagger-ui').version),
         "jinja2_version": "{}".format(pkg_resources.get_distribution('jinja2').version),
         "flask_version": "{}".format(pkg_resources.get_distribution('flask').version),
         "cffi_version": "{}".format(pkg_resources.get_distribution('cffi').version),
+        "pycparser_version": "{}".format(pkg_resources.get_distribution('pycparser').version),
         "click_version": "{}".format(pkg_resources.get_distribution('click').version),
         "cryptography_version": "{}".format(pkg_resources.get_distribution('cryptography').version),
         "itsdangerous_version": "{}".format(pkg_resources.get_distribution('itsdangerous').version),
         "jsonify_version": "{}".format(pkg_resources.get_distribution('jsonify').version),
+        "mysql-connector-python_version": "{}".format(pkg_resources.get_distribution('mysql-connector-python').version),
+        "protobuf_version": "{}".format(pkg_resources.get_distribution('protobuf').version),
+        "setuptools_version": "{}".format(pkg_resources.get_distribution('setuptools').version),
         "markupsafe_version": "{}".format(pkg_resources.get_distribution('markupsafe').version),
         "pycparser_version": "{}".format(pkg_resources.get_distribution('pycparser').version),
         "six_version": "{}".format(pkg_resources.get_distribution('six').version),
-        "werkzeug_version": "{}".format(pkg_resources.get_distribution('werkzeug').version),
-        "python_version": "{}.{}".format(version_info.major, version_info.minor)
+        "werkzeug_version": "{}".format(pkg_resources.get_distribution('werkzeug').version)
     })
 
 @app.route("/swagger_spec")
