f6057a8c85325e03bd92399b8fc32c952d9884f7
luko0610 <luko0610@gmail.com>
2020-05-31 19:26:04 +0200

    Added cors allow all origins for now.

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -3,7 +3,11 @@ The flask application package.
 """
 
 from flask import Flask
+from flask_cors import CORS
+
+
 app = Flask(__name__)
+cors = CORS(app, origins="*", supports_credentials=True)
 
 from api.models import db
 import api.models
