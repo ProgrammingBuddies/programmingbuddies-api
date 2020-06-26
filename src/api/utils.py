3c60a211dd8d3aa89f20779f2977cdde50d8dfa0
Adenylatcyclase <streicher1993@googlemail.com>
2020-06-26 12:48:36 +0200

    wrap_response can handle lists now

--- src/api/utils.py
+++ src/api/utils.py
@@ -3,6 +3,9 @@ from flask import jsonify
 def wrap_response(data, msg, code):
     obj = {"msg": msg}
     if not data is None:
-        obj["data"] = data.as_dict()
+        if type(data) == list:
+            obj["data"] = [dat.as_dict() for dat in data]
+        else:
+            obj["data"] = data.as_dict()
     return jsonify(obj), code
     
