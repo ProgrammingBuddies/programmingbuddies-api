0e427b69bb9e44fac217811aea02338d7fbd7e20
Roch D'Amour <roch.damour@gmail.com>
2020-06-25 23:12:49 -0400

    Generate DB tables before tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,8 +4,16 @@ import sys
 # todo: change this importing solution
 sys.path.insert(0, os.getcwd()+'/src')
 
+from os import environ
 from api import app
 from api.models import db
 from api.models import User, Project, UserFeedback, ProjectFeedback, UserLink, ProjectLink
+from api.models.database import init_db
+
+# Only create database table when testing in the CI.
+# When testing in CI, we asume the DB is fresh everytime the
+# tests runs, so we *have* to create all models (tables) inside of it.
+if environ.get('FLASK_ENV', 'false'):
+    init_db(True)
 
 sys.path.insert(0, os.getcwd()+'/tests')
